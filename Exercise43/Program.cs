476f51a7e25376545c868d15869d254fed882d72
Sergey <bibert.shmibert@gmail.com>
2022-10-06 15:45:51 +0400

    add exercise 43

---
+++ Exercise43/Program.cs
@@ -0,0 +1,9 @@
+﻿
+void CrossPoint(double b1, double k1, double b2, double k2)
+{
+    double x = ( b2 - b1 ) / (k1 - k2);
+    double y = (k1 * x) + b1;
+    Console.WriteLine($"Точка пересечения прямых - ({x}; {y})");
+}
+
+CrossPoint(2, 5, 4, 9);
